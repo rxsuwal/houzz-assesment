657c3435f70976173d6c0ed9e3754774ca532350
ranjan <work.rsuwal@gmail.com>
2023-08-07 23:36:19 +0545

    all products tab content completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css';
-import { Button, Card, Col, Nav, Row, Spinner, Tab, Tabs } from 'react-bootstrap';
+import { Button, Card, Col, Nav, OverlayTrigger, Row, Spinner, Tab, Tabs, Tooltip } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useCallback, useEffect, useId, useState } from 'react';
-import axios from "axios"
-import { getBeers } from './axios/beer';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useEffect, useId, useState } from 'react';
+import axios from "axios";
+import { ProductCard } from './component/ProductCard';
 
 
 
@@ -20,11 +21,16 @@ function App() {
     perPage: 2,
   })
 
+  const [loadMoreLoader, setLoadMoreLoader] = useState(false)
+
+
   const getBeers = (async (pagination) => {
     await axios.get(`https://api.punkapi.com/v2/beers?page=${pagination.page}&per_page=${pagination.perPage}`).then(res => {
       setallBeers({ data: allBeers.data.concat(res.data), loading: false })
     }).catch(err => {
       setallBeers({ data: err.response.data, loading: false })
+    }).then(() => {
+      setLoadMoreLoader(false)
     })
   })
 
@@ -33,7 +39,9 @@ function App() {
   }, [])
 
 
+
   const loadMore = () => {
+    setLoadMoreLoader(true)
     setPaginate({ ...paginate, page: paginate.page + 1 })
     getBeers(paginate)
   }
@@ -41,7 +49,7 @@ function App() {
 
   return (
     <div className="container">
-      <div className="w-75 m-auto">
+      <div className="w-md-75 m-auto">
         <Tab.Container id="left-tabs-example" defaultActiveKey="first">
           <Nav variant="pills" className=" mb-4">
             <Nav.Item>
@@ -55,7 +63,7 @@ function App() {
             <Tab.Pane eventKey="first">
 
               <div className="row">
-                {allBeers.loading && <Spinner className='m-auto' />}
+                {allBeers.loading && <Spinner className='m-auto my-4' />}
 
                 {!allBeers.loading && <>
                   {!allBeers.data.isArray ?
@@ -64,16 +72,19 @@ function App() {
                       {
                         allBeers.data?.map((a) => {
                           return (
-                            <div className="col-xl-6 mb-4 h-100" key={a.id}>
+                            <div className="col-xl-6 mb-4" key={a.id}>
                               <ProductCard data={a} />
                             </div>
                           )
                         })
                       }
-                      <button type="button" class="btn fw-bold text-primary m-auto"
+
+
+                      {loadMoreLoader && <Spinner className='m-auto my-4' />}
+                      {!loadMoreLoader && <button type="button" class="btn fw-bold text-primary m-auto"
                         onClick={() => loadMore()}>
-                        Load More
-                      </button>
+                        Load More <i class="bi bi-chevron-down fw-bolder"></i>
+                      </button>}
 
                     </>
                     : allBeers?.data}
@@ -92,19 +103,3 @@ function App() {
 
 export default App;
 
-export function ProductCard(props) {
-  return (
-    <a href='#' class="card flex-row border border-0 align-items-center shadow">
-      <img src={props.data.image_url}
-        style={{ height: "100px", objectFit: "contain" }}
-        class="card-img-top w-25" alt="product" />
-      <div class="card-body w-75">
-        <h5 class="card-title">{props.data.name}</h5>
-        <h6 className='text-warning'>{props.data.tagline}</h6>
-        <p class="card-text">
-          {props.data.description}
-        </p>
-      </div>
-    </a >
-  )
-}
